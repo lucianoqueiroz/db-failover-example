c8fc4a12dd28cd9678427877f4d34ddc704333c6
Luciano Queiroz <lucianoqueiroz@protonmail.com>
2021-08-28 15:59:07 +0200

    Add parameters to corrupt master db network script

--- scripts/corrupt-master-db-network.sh
+++ scripts/corrupt-master-db-network.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level debug netem --duration 5m corrupt --percent 80 db-failover-example_db-master_1
+# Corrupt packets from the `db-failover-example_db-master_1` Docker container
+
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level debug netem --duration $2 corrupt --percent $1 db-failover-example_db-master_1
